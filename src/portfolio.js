ccb82254b67c2adeec15c8c46d0c0914697c619c
yifan <ywang289@buffalo.edu>
2025-01-17 16:04:17 -0500

    add att project

--- src/portfolio.js
+++ src/portfolio.js
@@ -36,8 +36,7 @@ const socialMediaLinks = {
   github: "https://github.com/ywang289",
   linkedin: "https://www.linkedin.com/in/yifanwang00925",
   gmail: "yifan00925@gmail.com",
-  
- 
+
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -49,10 +48,10 @@ const skillsSection = {
   title: "What I do",
   subTitle: "FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
+    emoji("⚡ Implemented distrbuted system that handles high concurrency  "),
     emoji(
-      "⚡ Implemented distrbuted system that handles high concurrency  "
+      "⚡ Deployed the platfrom on Cloud Platform like AWS (Amplify, EC2, S3, Lambda, API Gateway), GCP, Azure"
     ),
-    emoji("⚡ Deployed the platfrom on Cloud Platform like AWS (Amplify, EC2, S3, Lambda, API Gateway), GCP, Azure"),
     emoji(
       "⚡  Develop highly interactive Front end / User Interfaces for your web and mobile applications"
     )
@@ -62,7 +61,6 @@ const skillsSection = {
 https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
-
     {
       skillName: "html-5",
       fontAwesomeClassname: "fab fa-html5"
@@ -109,14 +107,12 @@ https://fontawesome.com/icons?d=gallery */
     },
     {
       skillName: "Go",
-      fontAwesomeClassname: "fab fa-golang" 
+      fontAwesomeClassname: "fab fa-golang"
     },
     {
       skillName: "Figma",
-      fontAwesomeClassname: "fab fa-figma" 
-    },
- 
- 
+      fontAwesomeClassname: "fab fa-figma"
+    }
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -138,7 +134,7 @@ const educationInfo = {
       logo: require("./assets/images/buffaloLogo.png"),
       subHeader: "Bachelor of Science in Computer Science",
       duration: "January 2019 - May 2022",
-      desc: "Ranked top 10% in the program. Took courses about Computer Network, Algorithm and Design, DataBase,  Operating Systems ",
+      desc: "Ranked top 10% in the program. Took courses about Computer Network, Algorithm and Design, DataBase,  Operating Systems "
     }
   ]
 };
@@ -170,10 +166,17 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
+      role: "Back-End Engineer",
+      company: "AT&T",
+      companylogo: require("./assets/images/att.jpeg"),
+      date: "Jan 2025 – Present",
+      desc: "Developed the RAN Forecast Tool using C++ and PostgreSQL, enabling the performance forecasting of AT&T's 4G LTE and 5G NR network utilization."
+    },
+    {
       role: "Front-End Developer",
       company: "Torpago",
       companylogo: require("./assets/images/TorpagoLogo.png"),
-      date: "Sep 2024 – Present",
+      date: "Sep 2024 – Jan 2025",
       desc: "Implemented the frontend for the Torpago Payment System using SolidJS and used Golang and Kubernetes for cloud platform control."
     },
     {
@@ -181,16 +184,16 @@ const workExperiences = {
       company: "Voyapay",
       companylogo: require("./assets/images/VoyaLogo.png"),
       date: "Dec 2023 – Sep 2024",
-      desc: " Implemented a highly available architecture for the VoyaPay virtual credit card project, ensuring robust security, scalability, and maintainability of the system.",
-     
+      desc: " Implemented a highly available architecture for the VoyaPay virtual credit card project, ensuring robust security, scalability, and maintainability of the system."
     },
-    
+
     {
       role: "Full Stack Engineer",
       company: "The Donovan's Venom",
       companylogo: require("./assets/images/TheLogo.png"),
       date: "Sep 2023 – Dec 2023",
-      desc: "  Implemented a distributed system handling high concurrency, with support for over 10K users during peak hours, including payment processing functionalities for membership using NodeJS. "}
+      desc: "  Implemented a distributed system handling high concurrency, with support for over 10K users during peak hours, including payment processing functionalities for membership using NodeJS. "
+    }
   ]
 };
 
@@ -232,7 +235,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true// Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
